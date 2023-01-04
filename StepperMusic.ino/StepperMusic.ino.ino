be0d010048dc5762ce5db2789c34696055469725
Saina Koukpari <saina.koukpari@gmail.com>
2023-01-03 18:16:19 -0800

    Added Arduino File

---
+++ StepperMusic.ino/StepperMusic.ino.ino
@@ -0,0 +1,15 @@
+//Set Static Variables(notes)
+
+//Running Functions
+void setup() {
+  //setup code: output pins
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+}
+
+void loop() {
+  //main code: runs music for song
+  oct=5;
+}
+
+//Helper Functions
