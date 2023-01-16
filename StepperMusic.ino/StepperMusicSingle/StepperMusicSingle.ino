974aec10e3ba1fc90cc2e9e0eaee8046224febc5
Saina Koukpari <saina.koukpari@gmail.com>
2023-01-16 14:02:22 -0500

    complete note periods

--- StepperMusic.ino/StepperMusicSingle/StepperMusicSingle.ino
+++ StepperMusic.ino/StepperMusicSingle/StepperMusicSingle.ino
@@ -13,16 +13,16 @@ int tempo = 120;
 //the notes are arrays where the indices represent the note period at each octive
 unsigned long C[] = {61156, 30578, 15289, 7644, 3822, 1911, 955, 477, 238};
 unsigned long Cs[] = {57723, 28861, 14431, 7215, 3608, 1804, 902, 451, 225};
-unsigned long D[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-unsigned long Ds[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-unsigned long E[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+unsigned long D[] = {54484, 27242, 13621, 6810, 3405, 1703, 851, 426, 213};
+unsigned long Ds[] = {51426, 25712, 12856, 6428, 3214, 1607, 804, 402, 201};
+unsigned long E[] = {48540, 24270, 12134, 6067, 3034, 1517, 758, 379, 190};
 unsigned long F[] = {45815, 22907, 11453, 5726, 2863, 1431, 715, 357, 178};
-unsigned long Fs[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-unsigned long G[] = {0, 0, 0, 0, 0, 0, 637, 318, 159};
-unsigned long Gs[] = {0, 0, 0, 0, 0, 0, 602, 301, 150};
-unsigned long A[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-unsigned long As[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-unsigned long B[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+unsigned long Fs[] = {43244, 21622, 10810, 5405, 2703, 1351, 676, 338, 169};
+unsigned long G[] = {40817, 20408, 10204, 5102, 2551, 1275, 637, 318, 159};
+unsigned long Gs[] = {38526, 19263, 9631, 4816, 2408, 1204, 602, 301, 150};
+unsigned long A[] = {36364, 18182, 9091, 4545, 2272, 1136, 568, 284, 142};
+unsigned long As[] = {34323, 17161, 8581, 4290, 2145, 1072, 536, 268, 134};
+unsigned long B[] = {32396, 16198, 8099, 4050, 2024, 1012, 506, 253, 127};
 
 void setup(){
   //setup code: output pins for motor
